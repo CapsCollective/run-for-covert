d93c0bc30aedb05c8ae46970e641f1c475c6295e
Jonathan Moallem <jdmoallem@gmail.com>
2020-08-27 19:13:36 +1000

    Added functionality to AI controller to act on pathing

--- Source/RunForCovert/Controllers/EnemyAIController.cpp
+++ Source/RunForCovert/Controllers/EnemyAIController.cpp
@@ -23,11 +23,20 @@ void AEnemyAIController::BeginPlay()
 
     Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 
-    Character = Cast<AEnemyCharacterBase>(GetPawn());
+    Agent = Cast<AEnemyCharacterBase>(GetPawn());
 
     TimerHandle = FTimerHandle();
     GetWorldTimerManager().SetTimer(TimerHandle, this, &AEnemyAIController::FireAtPlayer,
                                     1.0f, true, 2.0f);
+
+    // Get a reference to the game mode (used as a service locator)
+    ADefaultGameModeBase* GameMode = Cast<ADefaultGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
+    if (!GameMode)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to find game mode"))
+        return;
+    }
+    CoverSystem = GameMode->GetCoverSystem();
 }
 
 
@@ -52,27 +61,40 @@ void AEnemyAIController::Tick(float DeltaTime)
 
     if (!Player) { return; }
 
+    // Uncomment this (and the line in FireAtPlayer) to see the AI constantly run and shoot at the player
     //MoveToActor(Player, 300.f);
 
-    // Uncomment this (and comment out the above) to test the cover system
-    if (LineOfSightTo(Player) && !CoverPosition) // TODO remember not to use LineOfSightTo as it disregards senses
+    // Uncomment this to see the AI constantly hide from the player
+//    if (LineOfSightTo(Player) && !CoverPosition) // TODO remember not to use LineOfSightTo as it disregards senses
+//    {
+//        // Get the closest valid cover point to the agent
+//        CoverPosition = CoverSystem->GetClosestValidCoverPoint(Agent, Player);
+//    }
+//    else if (CoverPosition)
+//    {
+//        // Move towards the player while there is a valid cover point
+//        MoveToLocation(CoverPosition->GetComponentLocation());
+//
+//        // Nullify the cover point once close enough
+//        if (FVector::Dist(Agent->GetActorLocation(), CoverPosition->GetComponentLocation()) < 1000.f)
+//        {
+//            CoverPosition = nullptr;
+//        }
+//    }
+
+    // Uncomment this to see the AI path towards the nearest cover to the player
+    if (CoverPath.Num() <= 0 && !CoverPosition)
     {
-        // Get a reference to the game mode (used as a service locator)
-        ACoverSystem* CoverSystem;
-        ADefaultGameModeBase* GameMode = Cast<ADefaultGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
-
-        if (!GameMode || !(CoverSystem = GameMode->GetCoverSystem())) { return; }
-
-        // Get the closest valid cover point to the agent
-        CoverPosition = CoverSystem->GetClosestValidCoverPoint(Character, Player);
+        CoverPath = CoverSystem->GetCoverPath(Agent, Player);
     }
-    else if (CoverPosition)
+    else if (!CoverPosition)
     {
-        // Move towards the player while there is a valid cover point
+        CoverPosition = CoverPath.Pop()->GetRandomCover();
+    }
+    else
+    {
         MoveToLocation(CoverPosition->GetComponentLocation());
-
-        // Nullify the cover point once close enough
-        if (FVector::Dist(Character->GetActorLocation(), CoverPosition->GetComponentLocation()) < 1000.f)
+        if (FVector::Dist(Agent->GetActorLocation(), CoverPosition->GetComponentLocation()) < 100.f)
         {
             CoverPosition = nullptr;
         }
@@ -81,8 +103,9 @@ void AEnemyAIController::Tick(float DeltaTime)
 
 void AEnemyAIController::FireAtPlayer()
 {
-    if (!Character) { return; }
+    if (!Agent) { return; }
 
+    // Uncomment this (and the line in Tick) to see the AI constantly run and shoot at the player
     //Character->FireWeapon();
 
     if (++RepeatedAction >= 15)
